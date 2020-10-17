d525d595b1efe1a7b036248d75e3bc9fec48ee59
SofiqurRahman <sofiqurr849@gmail.com>
2020-10-17 08:57:49 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+const fileUpload = require('express-fileupload');
+const MongoClient=require('mongodb').MongoClient;
+require('dotenv').config()
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3poo7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const app=express()
+app.use(bodyParser.json());
+app.use(cors())
+app.use(express.static('doctors'));
+app.use(fileUpload());
+const port=5000;
+app.get('/',(req,res)=>{
+  res.send('hello i am server');
+})
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const appointmentCollection = client.db("doctorsPortal").collection("appointments");
+
+  app.post('/addAppointment',(req,res)=>{
+    const appointment=req.body;
+    appointmentCollection.insertOne(appointment)
+    .then(result=>{
+      res.send(result.insertedCount>0);
+    })
+  })
+
+  app.post('/appointmentsByDate',(req,res)=>{
+    const date=req.body;
+    console.log(date.date);
+    appointmentCollection.find({date: date.date})
+    .toArray((err,documents)=>{
+      res.send(documents);
+    })
+  })
+
+  app.post('/addADoctor', (req, res) => {
+    const file = req.files.file;
+    const name = req.body.name;
+    const email = req.body.email;
+    console.log(name,email,file);
+      file.mv(`${__dirname}/doctors/${file.name}`, err=>{
+        if(err){
+          console.log(err);
+          return res.status(500).send({msg: 'Failed to upload image'});
+        }
+        return res.send({name: file.name,path: `/${file.name}`})
+      })
+    })
+});
+app.listen(process.env.PORT || port)
